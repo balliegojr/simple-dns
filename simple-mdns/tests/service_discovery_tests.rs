d9e26f294d8d7b383ca1fea6971384215a96140e
Ilson Roberto Balliego Junior <ilson.balliego@gmail.com>
2021-07-01 23:41:14 -0300

    improve service discovery test

--- simple-mdns/tests/service_discovery_tests.rs
+++ simple-mdns/tests/service_discovery_tests.rs
@@ -7,7 +7,7 @@ use std::{net::SocketAddr, time::Duration};
 fn get_oneshot_responder(srv_name: Name<'static>) -> SimpleMdnsResponder {
     let mut responder = SimpleMdnsResponder::default();
     let (r1, r2) =
-        socket_addr_to_srv_and_address(&srv_name, "192.168.1.3:8080".parse().unwrap(), 360);
+        socket_addr_to_srv_and_address(&srv_name, "192.168.1.4:8080".parse().unwrap(), 360);
     responder.add_resource(r1);
     responder.add_resource(r2);
 
@@ -27,18 +27,39 @@ fn service_discovery_can_find_services() {
     init_log();
     let _responder = get_oneshot_responder(Name::new_unchecked("_srv3._tcp.com"));
 
-    let mut service_discovery_a = ServiceDiscovery::new("_srv3._tcp.com", 360).unwrap();
+    std::thread::sleep(Duration::from_secs(1));
 
-    // service_discovery_a.add_socket_address("192.168.1.2:8080".parse().unwrap());
+    let mut service_discovery_a = ServiceDiscovery::new("_srv3._tcp.com", 360).unwrap();
+    let mut service_discovery_b = ServiceDiscovery::new("_srv3._tcp.com", 360).unwrap();
+
+    service_discovery_a.add_socket_address("192.168.1.2:8080".parse().unwrap());
+    service_discovery_b.add_socket_address("192.168.1.3:8080".parse().unwrap());
 
     std::thread::sleep(Duration::from_secs(3));
 
-    let services_a = service_discovery_a.get_known_services();
+    let mut services_a = service_discovery_a.get_known_services();
+    let mut services_b = service_discovery_b.get_known_services();
 
-    assert_eq!(1, services_a.len());
+    services_a.sort();
+    services_b.sort();
+
+    assert_eq!(2, services_a.len());
+    assert_eq!(2, services_b.len());
 
     assert_eq!(
         &("192.168.1.3:8080".parse::<SocketAddr>().unwrap()),
         &services_a[0]
+    );
+    assert_eq!(
+        &("192.168.1.4:8080".parse::<SocketAddr>().unwrap()),
+        &services_a[1]
+    );
+    assert_eq!(
+        &("192.168.1.2:8080".parse::<SocketAddr>().unwrap()),
+        &services_b[0]
+    );
+    assert_eq!(
+        &("192.168.1.4:8080".parse::<SocketAddr>().unwrap()),
+        &services_b[1]
     );
 }
