685d0d1c1b03fed76fe988cf75cefc2e254cb9bc
Sherry Fan <sherryfansf@berkeley.edu>
2023-09-13 18:20:27 -0700

    add ALL derived traits

--- simple-dns/src/dns/mod.rs
+++ simple-dns/src/dns/mod.rs
@@ -36,7 +36,7 @@ const MAX_NULL_LENGTH: usize = 65535;
 
 bitflags! {
     /// Possible Packet Flags
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
     pub struct PacketFlag: u16 {
         /// Indicates if this packet is a query or a response. This is the QR flag in the DNS
         /// specifications, this flag is called Response here to be more ergonomic
