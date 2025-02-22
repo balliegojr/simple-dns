1c353d610b771011e87053deb053310648eb74f0
Ilson Roberto Balliego Junior <ilson.balliego@gmail.com>
2025-02-22 21:21:39 +0100

    remove local-lib feature from the testing crate

--- bind9-tests/build.rs
+++ bind9-tests/build.rs
@@ -6,11 +6,6 @@ use bindgen::Builder;
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
 
-    if cfg!(feature = "local-lib") {
-        link_local("dns");
-        link_local("isc");
-    }
-
     println!("cargo:rustc-link-lib=dns");
     println!("cargo:rustc-link-lib=isc");
     println!("cargo:rustc-link-lib=atomic");
@@ -23,24 +18,8 @@ fn main() {
         .expect("Couldn't write bindings");
 }
 
-fn link_local(lib: &str) {
-    let libdir_path = PathBuf::from(format!("../bind9/lib/{lib}/.libs"))
-        .canonicalize()
-        .expect("bind9 directory not found");
-
-    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
-}
-
 fn get_bindings_builder() -> Builder {
-    let bindings = bindgen::Builder::default()
+    bindgen::Builder::default()
         .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
-
-    if cfg!(feature = "local-lib") {
-        bindings
-            .clang_arg("-I../bind9/lib/dns/include")
-            .clang_arg("-I../bind9/lib/isc/include")
-    } else {
-        bindings
-    }
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
 }
